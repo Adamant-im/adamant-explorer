e550ed93ff27dea20e83cb3237e8eb662ec3a767
Oliver Beddows <oliver.beddows@googlemail.com>
2015-04-17 10:06:02 +0100

    Adding /api/delegates/getActive.

--- api/delegates.js
+++ api/delegates.js
@@ -2,4 +2,11 @@ var delegates = require('../lib/api/delegates');
 
 module.exports = function (app) {
     var api = new delegates(app);
+
+    app.get("/api/delegates/getActive", function (req, res, next) {
+        api.getActive(
+            function (data) { res.json(data); },
+            function (data) { req.json = data; return next(); }
+        );
+    });
 }
