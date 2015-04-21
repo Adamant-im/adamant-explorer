1380e46d175a81f8185638bb4f4778b6848c2095
Oliver Beddows <oliver.beddows@googlemail.com>
2015-04-21 15:19:59 +0100

    Refactoring code.
    - Moving logic into separate constructors
    - Removing dependency on lib/api/delegates.js
    - Using async waterfall to collect query data

--- api/delegates.js
+++ api/delegates.js
@@ -2,12 +2,4 @@ var delegates = require('../lib/api/delegates');
 
 module.exports = function (app) {
     var api = new delegates(app);
-
-    app.get("/api/getDelegate", function (req, res, next) {
-        api.getDelegate(
-            req.query.publicKey,
-            function (data) { res.json(data); },
-            function (data) { req.json = data; return next(); }
-        );
-    });
 }
