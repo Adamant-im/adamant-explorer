a9f6d2a641ac36a8ccfb884e231ba9c65dd1ca7e
Oliver Beddows <oliver.beddows@googlemail.com>
2015-05-01 17:01:06 +0100

    Changing order of function(s).

--- api/statistics.js
+++ api/statistics.js
@@ -3,15 +3,15 @@ var statistics = require('../lib/api/statistics');
 module.exports = function (app) {
     var api = new statistics(app);
 
-    app.get('/api/statistics/getBlocks', function (req, res) {
-        api.getBlocks(
+    app.get('/api/statistics/getLastBlock', function (req, res) {
+        api.getLastBlock(
             function (data) { res.json(data); },
             function (data) { res.json(data); }
         );
     });
 
-    app.get('/api/statistics/getLastBlock', function (req, res) {
-        api.getLastBlock(
+    app.get('/api/statistics/getBlocks', function (req, res) {
+        api.getBlocks(
             function (data) { res.json(data); },
             function (data) { res.json(data); }
         );
