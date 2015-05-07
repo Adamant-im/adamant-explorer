b636b07e1b6f6b2213373a6561c28dba5ea91dff
Oliver Beddows <oliver.beddows@googlemail.com>
2015-05-07 11:31:13 +0100

    Updating angular-js to version: ~1.3.15.

--- public/src/js/services/global.js
+++ public/src/js/services/global.js
@@ -2,7 +2,7 @@
 
 // Global service for global variables
 angular.module('cryptichain.system')
-  .factory('Global', [ function () {} ])
+  .factory('Global', [ function () { return true; } ])
   .factory('Version',
     function ($resource) {
         return $resource('/api/version');
