264130db2eb81937dcf4c1c36d5164c9daacd0fc
Oliver Beddows <oliver.beddows@googlemail.com>
2015-07-28 21:24:01 +0100

    Adding services/marketWatcher.js.

---
+++ public/src/js/services/marketWatcher.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var MarketWatcher = function ($scope, stockChart) {
+    this.$scope = $scope;
+    this.chart  = stockChart($scope);
+}
+
+angular.module('cryptichain.tools').factory('marketWatcher',
+  function ($socket, stockChart) {
+      return function ($scope) {
+          var marketWatcher = new MarketWatcher($scope, stockChart),
+              ns = $socket('/marketWatcher');
+
+          ns.on('data', function (res) {
+          });
+
+          $scope.$on('$destroy', function (event) {
+              ns.removeAllListeners();
+          });
+
+          $scope.$on('$locationChangeStart', function (event, next, current) {
+              ns.emit('forceDisconnect');
+          });
+
+          return marketWatcher;
+      }
+  });
