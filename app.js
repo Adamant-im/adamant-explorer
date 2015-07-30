43bd3fe19e76edea34234bfbdb3e917643667cb4
Olivier Beddows <oliver.beddows@googlemail.com>
2015-07-30 13:51:20 +0100

    Indenting fragment to four spaces.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
 if (process.env.NODE_ENV == 'production') {
