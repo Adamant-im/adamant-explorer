113529b80b22cdedba8a95afea5af4ae746cc0d4
Olivier Beddows <oliver.beddows@googlemail.com>
2015-10-30 09:30:49 +0000

    Updating transaction types.

--- public/src/js/services/txTypes.js
+++ public/src/js/services/txTypes.js
@@ -6,5 +6,11 @@ angular.module('cryptichain.system').value('txTypes', {
     2 : 'Delegate registration',
     3 : 'Delegate vote',
     4 : 'Username registration',
-    5 : 'New contact'
+    5 : 'New contact',
+    6 : 'New message',
+    7 : 'New avatar',
+    8 : 'Multi-signature creation',
+    9  : 'Dapp registration',
+    10 : 'Dapp deposit',
+    11 : 'Dapp withdrawal'
 });
