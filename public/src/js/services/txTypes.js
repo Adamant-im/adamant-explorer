bf6eefa64a121b3015cec4eb7009777656082111
Olivier Beddows <olivier@lisk.io>
2016-04-29 17:10:25 +0000

    Updating transaction types.

--- public/src/js/services/txTypes.js
+++ public/src/js/services/txTypes.js
@@ -5,12 +5,8 @@ angular.module('lisk_explorer.system').value('txTypes', {
     1 : 'Second signature creation',
     2 : 'Delegate registration',
     3 : 'Delegate vote',
-    4 : 'Username registration',
-    5 : 'New contact',
-    6 : 'New message',
-    7 : 'New avatar',
-    8 : 'Multi-signature creation',
-    9  : 'Dapp registration',
-    10 : 'Dapp deposit',
-    11 : 'Dapp withdrawal'
+    4 : 'Multi-signature creation',
+    5 : 'Dapp registration',
+    7 : 'Dapp deposit',
+    8 : 'Dapp withdrawal'
 });
