9389338e77338ce9b621534a2e6c424fa8181539
Oliver Beddows <oliver@lisk.io>
2016-05-24 20:16:55 +0000

    Updating epoch timestamp for launch.

--- public/src/js/filters.js
+++ public/src/js/filters.js
@@ -16,7 +16,7 @@ angular.module('lisk_explorer')
   .filter('epochStamp', function () {
       return function (d) {
           return new Date(
-              (((Date.UTC(2015, 3, 9, 0, 0, 0, 0) / 1000) + d) * 1000)
+              (((Date.UTC(2016, 4, 24, 17, 0, 0, 0) / 1000) + d) * 1000)
           );
       };
   })
