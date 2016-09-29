0b162623aa56aa1f64d7a78eff9a98f34bc862c7
Oliver Beddows <oliver@lisk.io>
2016-09-29 20:18:58 +0200

    Hinting files.

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-}
+};
 app.use(allowCrossDomain);
 
 if (process.env.NODE_ENV === 'production') {
