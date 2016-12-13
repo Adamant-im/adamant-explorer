fd82ac2eaab2e6b45bf09d0f52a3357bc0689678
Isabello <isabella@lisk.io>
2016-12-13 13:28:09 -0600

    Add more tests to candles.js - improve endpoints

--- test/api/candles.js
+++ test/api/candles.js
@@ -17,13 +17,22 @@ describe("Candles API", function() {
   /*Define api endpoints to test */
   describe("GET /api/candles/getCandles", function() {
 
+        it('should be ok with no inputs', function(done) {
+                getCandles('','', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('exchange').to.be.equal('poloniex');
+                done();
+                 });
+        });
+
         it('should be ok with poloniex', function(done) {
                 getCandles('poloniex', '', function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('exchange').to.be.equal('poloniex');
                 node.expect(res.body).to.have.property('candles');
                 done();
                  });
-         });
+        });
 
         it('should not be ok with bittrex', function(done) {
                 getCandles('bittrex','', function(err, res) {
@@ -32,19 +41,53 @@ describe("Candles API", function() {
                  });
         });
 
+        it('should be ok with minute', function(done) {
+                getCandles('','minute', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('timeframe').to.be.equal('minute');
+                done();
+                 });
+        });
+
+        it('should be ok with hour', function(done) {
+                getCandles('','hour', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('timeframe').to.be.equal('hour');
+                done();
+                 });
+        });
+
+        it('should be ok with day', function(done) {
+                getCandles('','day', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('timeframe').to.be.equal('day');
+                done();
+                 });
+        });
+
+
   });
 
   /* -- if all fail, check lisk for topAccounts = true */
   describe("GET /api/candles/getStatistics", function() {
 
+        it('should be ok with no input', function(done) {
+                getStatistics('', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('exchange').to.be.equal('poloniex');
+                done();
+                });
+        });
+
         it('should be ok with poloniex', function(done) {
                 getStatistics('poloniex', function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('exchange').to.be.equal('poloniex');
                 done();
-                 });
-         });
+                });
+        });
 
-        it('should be not be ok bittrex', function(done) {
+        it('should not be ok bittrex', function(done) {
                 getStatistics('bittrex', function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.not.ok;
                 done();
