50cebcd37f72d5bd2e433da5d91cceebb5ea5bef
Isabello <isabella@lisk.io>
2016-12-21 16:45:58 -0600

    Enhance blocks.js endpoint tests

--- test/api/blocks.js
+++ test/api/blocks.js
@@ -3,19 +3,24 @@
 var node = require('./../node.js');
 
 /*expecting testnet genesis block for tests*/
-var block = {
-    blockHeight: 1,
-    id: 7807109686729042739,
+var params = {
+    height: 1,
+    id: '7807109686729042739',
+    id2: '16739814892588171993',
     generatorPublicKey: '73ec4adbd8f99f0d46794aeda3c3d86b245bd9d27be2b282cdd38ad21988556b',
     totalAmount: 100000000,
     totalFee: 0,
-};
+}
 
 describe("Blocks API", function() {
 
     /*Define functions for use within tests*/
     function getLastBlocks(id, done) {
         node.get('/api/getLastBlocks?n=' + id, done);
+    }
+
+		function getBlockStatus(done) {
+        node.get('/api/getBlockStatus', done);
     }
 
     function getBlock(id, done) {
@@ -26,14 +31,100 @@ describe("Blocks API", function() {
         node.get('/api/getHeight?height=' + id, done);
     }
 
+
+
+    function checkPagination(id) {
+        node.expect(id).to.have.property('currentPage');
+        node.expect(id).to.have.property('more');
+        node.expect(id).to.have.property('previousPage');
+        node.expect(id).to.have.property('before');
+        node.expect(id).to.have.property('nextPage');
+    }
+
+    function checkLastBlocks(id) {
+        for (var i = 0; i < id.length; i++) {
+            if (id[i + 1]) {
+                checkLastBlock(id[i]);
+                checkDelegate(id[i].delegate);
+            }
+        }
+    }
+
+    function checkLastBlock(id) {
+        node.expect(id).to.contain.all.keys(
+            'delegate',
+            'generator',
+            'reward',
+            'id',
+            'timestamp',
+            'height',
+            'transactionsCount',
+            'totalAmount',
+            'totalFee',
+            'totalForged'
+        );
+    }
+
+    function checkDelegate(id) {
+        node.expect(id).to.contain.all.keys(
+            'productivity',
+            'username',
+            'address',
+            'publicKey',
+            'vote',
+            'producedblocks',
+            'missedblocks',
+            'rate',
+            'approval'
+        );
+    }
+
+    function checkBlock(id) {
+        node.expect(id).to.have.all.keys(
+            'delegate',
+            'totalForged',
+            'confirmations',
+            'blockSignature',
+            'generatorId',
+            'generatorPublicKey',
+            'payloadHash',
+            'payloadLength',
+            'reward',
+            'id',
+            'version',
+            'timestamp',
+            'height',
+            'previousBlock',
+            'numberOfTransactions',
+            'totalAmount',
+            'totalFee',
+            'usd'
+        );
+    }
+
     /*Define api endpoints to test */
     describe("GET /api/getLastBlocks", function() {
 
         it('should be ok', function(done) {
             getLastBlocks('0', function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
-                node.expect(res.body).to.have.property('blocks').to.be.a('array');
+                node.expect(res.body).to.have.property('blocks').to.be.an('array');
                 node.expect(res.body.blocks.length).to.equal(20);
+                node.expect(res.body).to.have.property('pagination');
+                checkLastBlocks(res.body.blocks);
+                checkPagination(res.body.pagination);
+                done();
+            });
+        });
+
+        it('should be ok with offset of 20', function(done) {
+            getLastBlocks('20', function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('blocks').to.be.an('array');
+                node.expect(res.body).to.have.property('pagination');
+                node.expect(res.body.blocks.length).to.equal(20);
+                checkLastBlocks(res.body.blocks);
+                checkPagination(res.body.pagination);
                 done();
             });
         });
@@ -41,7 +132,7 @@ describe("Blocks API", function() {
 
     describe("GET /api/getBlockStatus", function() {
         it('should be ok', function(done) {
-            node.get('/api/getBlockStatus', function(err, res) {
+            getBlockStatus( function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
                 node.expect(res.body).to.have.property('broadhash').to.be.a('string');
                 node.expect(res.body).to.have.property('epoch').to.be.a('string');
@@ -59,10 +150,21 @@ describe("Blocks API", function() {
     describe("GET /api/getBlock", function() {
 
         it('using known blockId should be ok', function(done) {
-            getBlock('7807109686729042739', function(err, res) {
+            getBlock(params.id, function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
                 node.expect(res.body).to.have.property('block').to.be.a('object');
-                node.expect(res.body.block).to.have.property('id').to.be.a('string');
+                node.expect(res.body.block.delegate).to.be.null;
+                checkBlock(res.body.block);
+                done();
+            });
+        });
+
+        it('using known blockId @ Height 2 should be ok', function(done) {
+            getBlock(params.id2, function(err, res) {
+                node.expect(res.body).to.have.property('success').to.be.ok;
+                node.expect(res.body).to.have.property('block').to.be.a('object');
+                checkBlock(res.body.block);
+                checkDelegate(res.body.block.delegate);
                 done();
             });
         });
@@ -88,10 +190,10 @@ describe("Blocks API", function() {
     describe("GET /api/getHeight", function() {
 
         it('using known height be ok ', function(done) {
-            getHeight('1', function(err, res) {
+            getHeight(params.height, function(err, res) {
                 node.expect(res.body).to.have.property('success').to.be.ok;
                 node.expect(res.body).to.have.property('block').to.be.a('object');
-                node.expect(res.body.block.id).to.equal('7807109686729042739');
+                node.expect(res.body.block.id).to.equal(params.id);
                 done();
             });
         });
