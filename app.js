3b2d586f8ec591df83c6c107e3f4381c74f8010f
4miners <mariusz@serek.net>
2017-01-01 21:21:11 +0100

    Fixed jshint errors

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.set('orders enabled', config.enableOrders);
 app.use (function (req, res, next) {
     res.setHeader ('X-Frame-Options', 'DENY');
     res.setHeader ('X-Content-Type-Options', 'nosniff');
-    res.setHeader ('X-XSS-Protection', '1; mode=block')
+    res.setHeader ('X-XSS-Protection', '1; mode=block');
     var ws_src = (req.protocol === 'http' ? 'ws://' : 'wss://') + req.get('host');
-    res.setHeader ('Content-Security-Policy', "frame-ancestors 'none'; default-src 'self'; connect-src 'self' " + ws_src + "; img-src 'self' https://*.tile.openstreetmap.org; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com");
+    res.setHeader ('Content-Security-Policy', 'frame-ancestors \'none\'; default-src \'self\'; connect-src \'self\' ' + ws_src + '; img-src \'self\' https://*.tile.openstreetmap.org; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com');
     return next();
 });
 
