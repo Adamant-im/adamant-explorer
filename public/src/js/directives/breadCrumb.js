e47c9787b3e243c6ae6718aadf61e0f38a61b769
alihaghighatkhah <ali@lightcurve.io>
2017-03-27 19:24:20 +0200

    create template file for the breadcrmb directive, remove href from the last item

--- public/src/js/directives/breadCrumb.js
+++ public/src/js/directives/breadCrumb.js
@@ -4,7 +4,7 @@ angular.module('lisk_explorer')
     .directive('breadCrumb', ['$route', function ($route) {
         return {
             restric: 'E',
-            template: '<ol class="breadcrumb"><li data-ng-repeat="section in sections"><a data-ng-class="{active: !section.url}" href="{{section.url}}">{{section.title}}</a></li></ol>',
+            templateUrl: '/views/index/breadCrumb.html',
             link: function (scope, element, attrs) {
 
                 scope.init = function (e, next) {
@@ -13,10 +13,11 @@ angular.module('lisk_explorer')
                     var section = next.$$route;
                     while (section.parent !== section.title) {
                         for (route in $route.routes) {
+                            
                             if ($route.routes.hasOwnProperty(route) && $route.routes[route].title === section.parent) {
                                 scope.sections.unshift({
                                     title: $route.routes[route].title,
-                                    url: $route.routes[route].originalPath
+                                    url: $route.routes[route].originalPath.replace(/(?:\/\:([^\/]+)?)\?$/g, '')
                                 });
                                 section = $route.routes[route];
                                 break;
@@ -24,11 +25,13 @@ angular.module('lisk_explorer')
                         }
                     }
                     scope.sections.push({
-                        title: next.$$route.title
+                        title: next.$$route.title,
+                        url: '#'
                     });
                 }
 
-                scope.$on('$routeChangeStart', scope.init);
+                // scope.$on('$routeChangeStart', scope.init);
+                scope.$on('$routeChangeSuccess', scope.init);
             }
         };
     }
