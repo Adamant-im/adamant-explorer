913ae2922bbaec431b4e6e8b55ec837b3135b2d7
Oliver Beddows <oliver@lightcurve.io>
2017-03-28 15:01:46 +0200

    Standardising code

--- public/src/js/controllers/address.js
+++ public/src/js/controllers/address.js
@@ -1,33 +1,33 @@
 'use strict';
 
 angular.module('lisk_explorer.address').controller('AddressController',
-	function ($scope, $rootScope, $routeParams, $location, $http, addressTxs) {
-		$scope.getAddress = function () {
-			$http.get('/api/getAccount', {
-				params: {
-					address: $routeParams.address
-				}
-			}).then(function (resp) {
-				if (resp.data.success) {
-						$scope.address = resp.data;
-				} else {
-						throw 'Account was not found!';
-				}
-			}).catch(function (error) {
-				$location.path('/');
-			});
-		};
+  function ($scope, $rootScope, $routeParams, $location, $http, addressTxs) {
+      $scope.getAddress = function () {
+          $http.get('/api/getAccount', {
+              params: {
+                  address: $routeParams.address
+              }
+          }).then(function (resp) {
+              if (resp.data.success) {
+                  $scope.address = resp.data;
+              } else {
+                  throw 'Account was not found!';
+              }
+          }).catch(function (error) {
+              $location.path('/');
+          });
+      };
 
-		$scope.address = {
-			address: $routeParams.address
-		};
+      $scope.address = {
+          address: $routeParams.address
+      };
 
-		// Sets the filter for which transactions to display
-		$scope.filterTxs = function(direction) {
-			$scope.direction = direction;
-			$scope.txs = addressTxs($routeParams.address, direction);
-		};
+      // Sets the filter for which transactions to display
+      $scope.filterTxs = function (direction) {
+          $scope.direction = direction;
+          $scope.txs = addressTxs($routeParams.address, direction);
+      };
 
-		$scope.getAddress();
-		$scope.txs = addressTxs($routeParams.address);
-	});
+      $scope.getAddress();
+      $scope.txs = addressTxs($routeParams.address);
+  });
