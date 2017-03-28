a286654114d1b61dc45897fbcf9eb59576002a9d
Oliver Beddows <oliver@lightcurve.io>
2017-03-28 15:02:00 +0200

    Standardising code

--- public/src/js/controllers/delegate.js
+++ public/src/js/controllers/delegate.js
@@ -1,33 +1,33 @@
 'use strict';
 
 angular.module('lisk_explorer.address').controller('DelegateController',
-    function ($scope, $rootScope, $routeParams, $location, $http, addressTxs) {
-        $scope.getAddress = function () {
-            $http.get('/api/getAccount', {
-                params: {
-                    address: $routeParams.delegateId
-                }
-            }).then(function (resp) {
-                if (resp.data.success) {
-                    $scope.address = resp.data;
-                } else {
-                    throw 'Account was not found!';
-                }
-            }).catch(function (error) {
-                $location.path('/');
-            });
-        };
+  function ($scope, $rootScope, $routeParams, $location, $http, addressTxs) {
+      $scope.getAddress = function () {
+          $http.get('/api/getAccount', {
+              params: {
+                  address: $routeParams.delegateId
+              }
+          }).then(function (resp) {
+              if (resp.data.success) {
+                  $scope.address = resp.data;
+              } else {
+                  throw 'Account was not found!';
+              }
+          }).catch(function (error) {
+              $location.path('/');
+          });
+      };
 
-        $scope.address = {
-            address: $routeParams.delegateId
-        };
+      $scope.address = {
+          address: $routeParams.delegateId
+      };
 
-        // Sets the filter for which transactions to display
-        $scope.filterTxs = function(direction) {
-            $scope.direction = direction;
-            $scope.txs = addressTxs($routeParams.delegateId, direction);
-        };
+      // Sets the filter for which transactions to display
+      $scope.filterTxs = function(direction) {
+          $scope.direction = direction;
+          $scope.txs = addressTxs($routeParams.delegateId, direction);
+      };
 
-        $scope.getAddress();
-        $scope.txs = addressTxs($routeParams.delegateId);
-    });
+      $scope.getAddress();
+      $scope.txs = addressTxs($routeParams.delegateId);
+  });
