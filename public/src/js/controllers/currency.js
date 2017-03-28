5623dda60db3b335e90911f1df99a568632170a4
Oliver Beddows <oliver@lightcurve.io>
2017-03-28 15:14:14 +0200

    Standardising code

--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module ('lisk_explorer.currency').controller ('CurrencyController',
+angular.module('lisk_explorer.currency').controller ('CurrencyController',
   function ($scope, $rootScope) {
-    $rootScope.currency.symbol = localStorage && localStorage.getItem ('lisk_explorer-currency') || 'LSK';
+      $rootScope.currency.symbol = localStorage && localStorage.getItem ('lisk_explorer-currency') || 'LSK';
 
-    $scope.setCurrency = function(currency) {
-      $rootScope.currency.symbol = currency;
-      if (localStorage) {
-        localStorage.setItem ('lisk_explorer-currency', currency);
-      }
-    };
+      $scope.setCurrency = function(currency) {
+          $rootScope.currency.symbol = currency;
+          if (localStorage) {
+              localStorage.setItem ('lisk_explorer-currency', currency);
+          }
+      };
   });
