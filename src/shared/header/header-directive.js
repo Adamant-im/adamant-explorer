8e1c3062ba2e3a959922c26caa2718174642fec3
alihaghighatkhah <ali@lightcurve.io>
2017-04-08 18:03:36 +0200

    use mainHeader directive instead of ngController, change the name of header service to CamelCase

--- src/shared/header/header-directive.js
+++ src/shared/header/header-directive.js
@@ -3,34 +3,36 @@
  * @todo Fix the service usage
  * 
  */
-const HeaderLink = () => {
-    $rootScope.currency = {
-      symbol: 'LSK'
+
+
+angular.module('lisk_explorer.system').directive('mainHeader', ($socket, $rootScope, Header) => {
+    const HeaderLink = () => {
+        $rootScope.currency = {
+        symbol: 'LSK'
+        };
+
+        const header = new Header($rootScope);
+        let ns = $socket('/header');
+
+        ns.on('data', res => {
+            if (res.status) { header.updateBlockStatus(res.status); }
+            if (res.ticker) { header.updatePriceTicker(res.ticker); }
+        });
+
+        ns.on('delegateProposals', res => {
+            if (res) { header.updateDelegateProposals(res); }
+        });
+
+
+        $rootScope.$on('$destroy', event => {
+            ns.removeAllListeners();
+        });
     };
 
-    const header = new Header($rootScope), ns = $socket('/header');
-
-    ns.on('data', res => {
-        if (res.status) { header.updateBlockStatus(res.status); }
-        if (res.ticker) { header.updatePriceTicker(res.ticker); }
-    });
-
-    ns.on('delegateProposals', res => {
-        if (res) { header.updateDelegateProposals(res); }
-    });
-
-
-    $rootScope.$on('$destroy', event => {
-        ns.removeAllListeners();
-    });
-};
-
-angular.module('lisk_explorer.system').directive('mainHeader', ($socket, $rootScope, header) => {
     return {
         restrict: 'E',
+        replace: true,
         link: HeaderLink,
         templateUrl: '/shared/header/header.html'
     }
 });
-
-
