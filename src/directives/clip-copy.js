82ca64319f89f209111857e1e776a37c2c5239db
alihaghighatkhah <ali@lightcurve.io>
2017-05-10 22:59:28 +0200

    Fix the issue with clip-copy directive

--- src/directives/clip-copy.js
+++ src/directives/clip-copy.js
@@ -1,36 +1,28 @@
-'use strict';
+import AppTools from '../app/app-tools.module.js';
+import ZeroClipboard from 'zeroclipboard';
+import ZeroClipboardSwf from '../assets/swf/ZeroClipboard.swf';
 
-const ZeroClipboard = window.ZeroClipboard;
 
-angular.module('lisk_explorer')
-  .directive('clipCopy', () => {
-      ZeroClipboard.config({
-          moviePath: '/swf/ZeroClipboard.swf',
-          trustedDomains: ['*'],
-          allowScriptAccess: 'always',
-          forceHandCursor: true
-      });
+AppTools.directive('clipCopy', () => {
+    return {
+        restric: 'A',
+        scope: { clipCopy: '=clipCopy' },
+        template: '<div class="tooltip fade right in"><div class="tooltip-arrow"></div><div class="tooltip-inner">Copied!</div></div>',
+        link: function (scope, elm) {
+            const clip = new ZeroClipboard(elm);
 
-      return {
-          restric: 'A',
-          scope: { clipCopy: '=clipCopy' },
-          template: '<div class="tooltip fade right in"><div class="tooltip-arrow"></div><div class="tooltip-inner">Copied!</div></div>',
-          link: function (scope, elm) {
-              const clip = new ZeroClipboard(elm);
+            clip.on('ready', clp => {
+                const onAfterCopy = client => {
+                    clp.client.setText(scope.clipCopy);
+                };
 
-              clip.on('load', client => {
-                  const onMousedown = client => {
-                      client.setText(scope.clipCopy);
-                  };
+                clp.client.on('aftercopy', onAfterCopy);
+                scope.$on('$destroy', () => {
+                    clp.client.off('aftercopy', onAfterCopy);
+                });
+            });
 
-                  client.on('mousedown', onMousedown);
-
-                  scope.$on('$destroy', () => {
-                      client.off('mousedown', onMousedown);
-                  });
-              });
-
-              clip.on('noFlash wrongflash', () => elm.remove());
-          }
-      };
-  });
+            clip.on('noFlash wrongflash', () => elm.remove());
+        }
+    };
+});
