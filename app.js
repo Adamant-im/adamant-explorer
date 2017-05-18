b9c8105243c27a5e05c79f6132166bf426d51073
Isabello <isabella@lisk.io>
2017-05-18 14:33:21 +0000

    Temporarily disable CSP

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use (function (req, res, next) {
     res.setHeader ('X-Content-Type-Options', 'nosniff');
     res.setHeader ('X-XSS-Protection', '1; mode=block');
     var ws_src = 'ws://' + req.get('host') + ' wss://' + req.get('host');
-    res.setHeader ('Content-Security-Policy', 'frame-ancestors \'none\'; default-src \'self\'; connect-src \'self\' ' + ws_src + '; img-src \'self\' https://*.tile.openstreetmap.org; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com');
+   // res.setHeader ('Content-Security-Policy', 'frame-ancestors \'none\'; default-src \'self\'; connect-src \'self\' ' + ws_src + '; img-src \'self\' https://*.tile.openstreetmap.org; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com');
     return next();
 });
 
