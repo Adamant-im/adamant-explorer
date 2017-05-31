7aeeed8d24307a4ce5adf17742bb1a9e35a53b37
Mariusz Serek <mariusz@serek.net>
2017-05-31 15:10:42 +0200

    Grunt should break if there is an error

--- Gruntfile.js
+++ Gruntfile.js
@@ -195,9 +195,6 @@ module.exports = function (grunt) {
         }
     });
 
-    // Making grunt default to force in order not to break the project.
-    grunt.option('force', true);
-
     // Default task(s).
     grunt.registerTask('default', ['watch']);
 
