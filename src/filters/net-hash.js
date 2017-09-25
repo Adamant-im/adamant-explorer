e60e95fd3b95a3e0b408fbb73d7982bd0784d7cb
Zyuhel <zyuhel@yandex.ru>
2017-09-26 01:00:03 +0300

    change nethash

--- src/filters/net-hash.js
+++ src/filters/net-hash.js
@@ -4,9 +4,9 @@ import AppFilters from './filters.module';
  * @todo check the possibility of removing hard coded hashes
  */
 AppFilters.filter('nethash', () => nethash => {
-    if (nethash === 'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba') {
+    if (nethash === '38f153a81332dea86751451fd992df26a9249f0834f72f58f84ac31cceb70f43') {
         return 'Testnet';
-    } else if (nethash === 'ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511')  {
+    } else if (nethash === '77265cf40a806763bc1e3ff0d899a1c0582b46e84ce8808b445dd9b95aa86da5')  {
         return 'Mainnet';
     } else {
         return 'Local';
