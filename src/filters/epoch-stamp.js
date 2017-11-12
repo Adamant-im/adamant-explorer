53aead93cab3e6e4be74612537d1af34894d71a5
Zyuhel <zyuhel@yandex.ru>
2017-11-12 09:24:20 +0300

    change epoch time to correspond with network time

--- src/filters/epoch-stamp.js
+++ src/filters/epoch-stamp.js
@@ -1,5 +1,5 @@
 import AppFilters from './filters.module';
 
 AppFilters.filter('epochStamp', () => d => new Date(
-    (((Date.UTC(2016, 4, 24, 17, 0, 0, 0) / 1000) + d) * 1000)
+    (((Date.UTC(2017, 8, 2, 17, 0, 0, 0) / 1000) + d) * 1000)
 ));
