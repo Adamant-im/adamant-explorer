128efd07bda62fe72f26f61fe2cc2ad922c4f675
confleux <confleux@gmail.com>
2022-11-23 00:52:18 +0300

    fix: restore .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:mocha/recommended'],
+  plugins: ['mocha'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'require-jsdoc': ['off'],
+    'new-cap': ['off'],
+    camelcase: ['off'],
+    'max-len': [
+      'error',
+      {
+        code: 300,
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': ['off'],
+  },
+};
